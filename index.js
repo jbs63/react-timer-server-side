41e1ab22a5a99f79f57a7f7325c812446b09b564
Jeffrey Bates <jab246@students.uwf.edu>
2024-04-06 21:14:16 -0500

    fix: listen edit for heroku

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ configureSecurity(fastify);
 
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ port: 3000, host: '0.0.0.0' });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
