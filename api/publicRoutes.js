a0bf3358853f0bdf666de13ff1c0f75a3c13148a
Jeffrey Bates <jab246@students.uwf.edu>
2024-04-13 02:05:51 -0500

    feat: web browser profile lookup

--- api/publicRoutes.js
+++ api/publicRoutes.js
@@ -1,10 +1,45 @@
+// Create database connection and import account & shot time schemas
+require("../models/db.js");
+const Account = require("../models/account.js");
+const ShotTime = require("../models/shotTime.js");
+
 const publicRoutes = (instance, opts, done) => {
+
     instance.get("/", async (request, reply) => {
-        return {
+        /*return {
             message:
                 "This is a public endpoint. Request /protected to test the Clerk auth middleware",
-        };
+        };*/
+        return reply.view("./views/home", {});
     });
+
+    instance.post("/profile", async (request, reply) => {
+        let user = await Account.findOne({ username: request.body.username }).lean();
+        if(user) {
+            user = {
+                username: user.username,
+                avatar: user.avatarUrl,
+                fastestRT: user.fastestRT,
+                fastestDrills: user.fastestDrills
+            };
+        }
+        return reply.view("./views/profile", {user: user});
+    });
+
+    instance.get("/:username", async (request, reply) => {
+        const { username } = request.params;
+        let user = await Account.findOne({ username: username }).lean();
+        if(user) {
+            user = {
+                username: user.username,
+                avatar: user.avatarUrl,
+                fastestRT: user.fastestRT,
+                fastestDrills: user.fastestDrills
+            };
+        }
+        return reply.view("./views/profile", {user: user});
+    });
+
     done();
 };
 
--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const configureSecurity = require('./controllers/fastify-security.js');
 const privateRoutes = require('./api/privateRoutes.js');
 const publicRoutes = require('./api/publicRoutes.js');
 
+// Configure handlebars rendering engine
+fastify.register(require("@fastify/view"), {
+  engine: {
+    handlebars: require("handlebars")
+  },
+  layout: "./views/layouts/main.hbs",
+  includeViewExtension: true
+});
+fastify.register(require('@fastify/formbody'));
+
 // Register the created routes
 fastify.register(privateRoutes);
 fastify.register(publicRoutes);
